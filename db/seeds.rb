8e13c47435a6d54097fea5461c76e012e1d9f590
Kyle So <kyle.p.so@gmail.com>
2019-03-04 10:35:15 -0800

    add seed for idea

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Idea.destroy_all
+
+50.times do
+  created_at = Faker::Date.backward(365 * 2)
+
+  idea = Idea.create(
+    title: Faker::TvShows::BojackHorseman.quote,
+    description: Faker::Lorem.paragraph,
+    created_at: created_at,
+    updated_at: created_at,
+  )
+end
