a2f601bc1df10202b358c205c8d7756f5a39fae0
Kyle So <kyle.p.so@gmail.com>
2019-03-04 10:44:10 -0800

    add show, index, edit, and update actions on idas controller with corresponding view templates

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,4 +1,6 @@
 class IdeasController < ApplicationController
+  before_action :find_idea, only: [:show, :edit, :update, :destroy]
+
   def new
     @idea = Idea.new
   end
@@ -12,9 +14,31 @@ class IdeasController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def index
+    @ideas = Idea.all.order(Created_at: :desc)
+  end
+
+  def edit
+  end
+
+  def update
+    if @idea.update idea_params
+      redirect_to (idea_path(@idea))
+    else
+      rnder :edit
+    end
+  end
+
   private
 
   def idea_params
     params.require(:idea).permit(:title, :description)
+  end
+
+  def find_idea
+    @idea = Idea.find(params[:id])
   end
 end
