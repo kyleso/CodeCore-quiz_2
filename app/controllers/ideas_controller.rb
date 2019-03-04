43c7b88abff326383497091cab447375fdfe04e9
Kyle So <kyle.p.so@gmail.com>
2019-03-04 10:05:32 -0800

    add tests for new action on idea controller, and new action on idea controller

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,2 +1,11 @@
 class IdeasController < ApplicationController
+  def new
+    @idea = Idea.new
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:title, :description)
+  end
 end
--- spec/controllers/ideas_controller_spec.rb
+++ spec/controllers/ideas_controller_spec.rb
@@ -1,5 +1,14 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe IdeasController, type: :controller do
-
+  describe "#new" do
+    it "renders the new idea template" do
+      get(:new)
+      expect(response).to(render_template(:new))
+    end
+    it "sets an instance variable with a new idea" do
+      get(:new)
+      edpect(assigns(:idea)).to(be_a_new(Idea))
+    end
+  end
 end
